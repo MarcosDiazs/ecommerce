f52c61d78ea759834742f8c6288937434b838e1c
marcosdiazs <marcosdiazs1995@gmail.com>
2017-05-27 15:05:52 -0300

    atualização

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
-<?php 
+<?php
 
 require_once("vendor/autoload.php");
+
+//preciso mudar isso
+require_once("vendor/hcodebr/php-classes/src/DB/Sql.php");
+//fim da gambs
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	echo "OK";
 
+	$sql = new Sql();
+
+  $results = $sql->select("SELECT * FROM tb_users");
+
+  echo json_encode($results);
 });
 
 $app->run();
